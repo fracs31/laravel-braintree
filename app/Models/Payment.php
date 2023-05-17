1ed85574b40d657187bccd7e41e3197c0e992e38
fracs31 <pist.fracs95@gmail.com>
2023-05-17 14:25:47 +0200

    ADD: Payment's mass assignment (fillable)

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+    //Mass Assignment
+    protected $fillable = [
+        'amount',
+        'date',
+        'card_type',
+        'cardholder_name',
+        'card_number',
+        'card_expiration',
+        'card_cvv'
+    ];
 }
