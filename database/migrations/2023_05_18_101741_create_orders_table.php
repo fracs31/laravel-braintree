9a7c0eee77f597332601eec4113cbda70b40201f
fracs31 <pist.fracs95@gmail.com>
2023-05-18 12:28:39 +0200

    ADD: Order's migration

--- database/migrations/2023_05_18_101741_create_orders_table.php
+++ database/migrations/2023_05_18_101741_create_orders_table.php
@@ -14,8 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); //id
+            $table->dateTime("date"); //data
+            $table->boolean("status")->default(0); //stato
+            $table->string("first_name", 100); //nome del cliente
+            $table->string("last_name", 100); //cognome del cliente
+            $table->string("email"); //email del cliente
+            $table->string("phone", 20); //telefono del cliente
+            $table->string("address", 255); //indirizzo del cliente
+            $table->string("cap", 5); //cap del cliente
         });
     }
 
